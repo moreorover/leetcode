a5c2988a16af718c2c82dae4e4253cf3922380ac
Martynas <mselvenis@gmail.com>
2025-07-27 00:01:12 +0100

    FindFirstOccurrence

--- src/main/java/org/example/easy/strings/FindFirstOccurrence.java
+++ src/main/java/org/example/easy/strings/FindFirstOccurrence.java
@@ -6,11 +6,9 @@ public class FindFirstOccurrence {
         if (needle.length() > haystack.length()) return -1;
         if (needle.isEmpty()) return 0;
 
-        for (int i = 0; i <= haystack.length(); i++) {
-            if (i + needle.length() <= haystack.length()) {
-                String sub = haystack.substring(i, i + needle.length());
-                if (sub.equals(needle)) return i;
-            }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            String sub = haystack.substring(i, i + needle.length());
+            if (sub.equals(needle)) return i;
         }
 
         return -1;
