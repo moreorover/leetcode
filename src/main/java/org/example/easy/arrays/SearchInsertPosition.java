28d7446f4154caac9d6bedb233eacded3f1cf33b
Martynas <mselvenis@gmail.com>
2025-07-27 00:25:53 +0100

    SearchInsertPosition optimized

--- src/main/java/org/example/easy/arrays/SearchInsertPosition.java
+++ src/main/java/org/example/easy/arrays/SearchInsertPosition.java
@@ -4,12 +4,7 @@ public class SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] >= target) return i;
-            if (i + 1 < nums.length) {
-                if (target < nums[i + 1]) return i + 1;
-            } else {
-                return i + 1;
-            }
         }
-        return -1;
+        return nums.length;
     }
 }
