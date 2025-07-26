e401f9102e5eff03de86669d2341b0b2ee313b4f
Martynas <mselvenis@gmail.com>
2025-07-26 17:02:49 +0100

    LongestCommonPrefix

---
+++ src/main/java/org/example/strings/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package org.example.strings;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char temp = strs[0].charAt(i);
+            for (String str : strs) {
+                if (str.length() <= i) return result.toString();
+
+                if (str.charAt(i) != temp) {
+                    return result.toString();
+                }
+            }
+            result.append(temp);
+        }
+        return result.toString();
+    }
+}
