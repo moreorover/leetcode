e5208087c09eb4c368a5f31436be4aa1e1989240
Martynas <mselvenis@gmail.com>
2025-07-27 00:38:29 +0100

    LengthOfLastWord optimized

--- src/main/java/org/example/easy/strings/LengthOfLastWord.java
+++ src/main/java/org/example/easy/strings/LengthOfLastWord.java
@@ -1,13 +1,10 @@
 package org.example.easy.strings;
 
-import java.util.Arrays;
-
 public class LengthOfLastWord {
     public static int lengthOfLastWord(String s) {
         s = s.trim();
         if (s.isEmpty()) return 0;
-        String[] x = s.split(" ");
-        String[] c = Arrays.stream(x).filter(v -> !v.isBlank()).map(String::trim).toArray(String[]::new);
-        return c[c.length - 1].length();
+        int lastSpace = s.lastIndexOf(' ');
+        return s.length() - lastSpace - 1;
     }
 }
