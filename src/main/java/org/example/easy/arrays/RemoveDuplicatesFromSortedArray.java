ba11dee16c5c23afe604f64db63cdbec9dae92f4
Martynas <mselvenis@gmail.com>
2025-07-26 22:57:54 +0100

    RemoveDuplicatesFromSortedArray

--- src/main/java/org/example/easy/arrays/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/org/example/easy/arrays/RemoveDuplicatesFromSortedArray.java
@@ -2,7 +2,18 @@ package org.example.easy.arrays;
 
 public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicates(int[] nums) {
-        // TODO: Implement logic
-        return 0;
+        int count = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (fast + 1 < nums.length) {
+                if (nums[fast] != nums[fast + 1]) {
+                    nums[count] = nums[fast];
+                    count++;
+                }
+            } else {
+                nums[count] = nums[fast];
+                count++;
+            }
+        }
+        return count;
     }
 }
