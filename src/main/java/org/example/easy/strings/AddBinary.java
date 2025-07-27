0fa2490a0437ef758f3ce5a6172040900aa94f4c
Martynas <mselvenis@gmail.com>
2025-07-27 10:35:22 +0100

    AddBinary

---
+++ src/main/java/org/example/easy/strings/AddBinary.java
@@ -0,0 +1,8 @@
+package org.example.easy.strings;
+
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+        // TODO: Implement logic
+        return "";
+    }
+}
