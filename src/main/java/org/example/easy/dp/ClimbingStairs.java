f9104e361eb7babe12553782cf0d9bfba672fb54
Martynas <mselvenis@gmail.com>
2025-07-27 12:08:46 +0100

    ClimbingStairs fixed

--- src/main/java/org/example/easy/dp/ClimbingStairs.java
+++ src/main/java/org/example/easy/dp/ClimbingStairs.java
@@ -3,9 +3,15 @@ package org.example.easy.dp;
 public class ClimbingStairs {
     public static int climbStairs(int n) {
         if (n <= 2) return n;
+
+        int prev1 = 2;
+        int prev2 = 1;
         int result = 0;
-        for (int i = 1; i <= n; i++) {
-            result += n - i;
+
+        for (int i = 3; i <= n; i++) {
+            result = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = result;
         }
         return result;
     }
