fcc0bf86b9a4ef5cc024eabbeb0730482f9b8ac1
Martynas <mselvenis@gmail.com>
2025-07-27 10:22:12 +0100

    PlusOne optimized

--- src/main/java/org/example/easy/arrays/PlusOne.java
+++ src/main/java/org/example/easy/arrays/PlusOne.java
@@ -2,26 +2,17 @@ package org.example.easy.arrays;
 
 public class PlusOne {
     public static int[] plusOne(int[] digits) {
-        int carry = 1;
         for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] == 9) {
-                digits[i] = 0;
-                carry = 1;
-                continue;
-            }
             if (digits[i] < 9) {
-                digits[i] = digits[i] + carry;
-                carry = 0;
+                digits[i]++;
+                return digits;
             }
+            digits[i] = 0;
         }
-        if (carry > 0) {
-            int[] result = new int[digits.length + 1];
-            result[0] = carry;
-            for (int i = 0; i < digits.length; i++) {
-                result[i + 1] = digits[i];
-            }
-            return result;
-        }
-        return digits;
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
     }
 }
